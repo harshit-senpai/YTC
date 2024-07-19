3b3d8a69ebc76f6cfb1944befc28fc15130c1ac3
harshit Mishra <miananya2@gmail.com>
2024-07-20 00:47:41 +0530

    getVideoData

---
+++ utils/getVideoData.ts
@@ -0,0 +1,54 @@
+const YT_INITIAL_PLAYER_RESPONSE_REGEX =
+  /ytInitialPlayerResponse\s*=\s*({.+?})\s*;\s*(?:var\s+(?:meta|head)|<\/script|\n)/
+
+function compareTracks(track1, track2) {
+  const langCode1 = track1.languageCode
+  const langCode2 = track2.languageCode
+
+  if (langCode1 === "en" && langCode2 !== "en") {
+    return -1
+  } else if (langCode1 !== "en" && langCode2 === "en") {
+    return 1
+  } else if (track1.kind !== "asr" && track2.kind === "asr") {
+    return -1
+  } else if (track1.kind === "asr" && track2.kind !== "asr") {
+    return 1
+  }
+
+  return 0
+}
+
+export async function getVideoData(id: string) {
+  //@ts-ignore
+  let player = window.ytInitialPlayerResponse
+  if (!player || id !== player.videoDetails.videoId) {
+    const pageData = await fetch(`https://www.youtube.com/wathc?v=${id}`)
+    const body = await pageData.text()
+
+    const playerResponseMatch = body.match(YT_INITIAL_PLAYER_RESPONSE_REGEX)
+
+    if (!playerResponseMatch) {
+      console.warn("unable to parse")
+      return
+    }
+
+    player = JSON.parse(playerResponseMatch[1])
+  }
+  const metaData = {
+    title: player.videoDetails.title,
+    duration: player.videoDetails.lengthSeconds,
+    author: player.videoDetails.author,
+    views: player.videoDetails.viewCount
+  }
+
+  if (player.captions && player.captions.playerCaptionsTracklistRenderer) {
+    const tracks = player.captions.playerCaptionsTracklistRenderer.captionTracks
+    if (tracks && tracks.length > 0) {
+      tracks.sort(compareTracks)
+      const transcriptResponse = await fetch(tracks[0].baseUrl + "&ft=json3")
+      const transcript = await transcriptResponse.json()
+      return { metaData, transcript }
+    }
+  }
+  return { metaData, transcript: null }
+}
